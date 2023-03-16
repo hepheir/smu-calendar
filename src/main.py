aae33dbd2fdd7373d276de68fe4d2d0812350935
Hepheir <hepheir@gmail.com>
2023-03-17 00:11:35 +0900

    refactor: 매직 변수의 분리 및 예외처리 로직 강화

--- src/main.py
+++ src/main.py
@@ -4,8 +4,12 @@ import typing
 import requests
 import ics
 
-calendar_url = "https://www.smu.ac.kr/ko/life/academicCalendar.do"
-calendar_ics_path = "docs/calendar.ics"
+################################################################################
+
+ICS_AUTHOR = "상명대학교 (+hepheir@gmail.com)"
+ICS_FILE_OUTPUT_PATH = "docs/calendar.ics"
+
+################################################################################
 
 def cvtStr2Datetime(datestring: str, pattern: str = '%Y-%m-%d') -> datetime.datetime:
     return datetime.datetime.strptime(datestring, pattern)
@@ -13,48 +17,46 @@ def cvtStr2Datetime(datestring: str, pattern: str = '%Y-%m-%d') -> datetime.date
 def cvtInt2Datetime(ms: int) -> datetime.datetime:
     return datetime.datetime(1970, 1, 1) + datetime.timedelta(milliseconds=ms)
 
-def loadCalendarJSON() -> typing.Dict[str, typing.Any]:
+def loadCalendarJSON(year: int) -> typing.Dict[str, typing.Any]:
     response = requests.post(
         url="https://www.smu.ac.kr/app/common/selectDataList.do",
         data={
             'sqlId': 'jw.Article.selectCalendarArticle',
             'modelNm': 'list',
-            'jsonStr': '{"year":"2023","bachelorBoardNoList":["85"]}',
+            'jsonStr': f'{{"year":"{year}","bachelorBoardNoList":["85"]}}',
         }
     )
     return response.json()
 
-def buildCalendarICS(calendarJSON) -> ics.icalendar.Calendar:
+def parseCalendarJSON(calendarJSON: typing.Dict[str, typing.Any]) -> typing.Iterable[ics.event.Event]:
     if not calendarJSON["success"]:
         return
-    calendar =  ics.icalendar.Calendar(creator="상명대학교 (+hepheir@gmail.com)")
     for eventJSON in calendarJSON["list"]:
-        event = ics.event.Event(
-            name = eventJSON["articleTitle"],
-            begin = cvtStr2Datetime(eventJSON["etcChar6"]),
-            end = cvtStr2Datetime(eventJSON["etcChar7"]),
-            uid = str(eventJSON["articleNo"]),
-            # description: str = None,
-            created = cvtInt2Datetime(eventJSON["createDt"]),
-            last_modified = cvtInt2Datetime(eventJSON["updateDt"]),
-            # location: str = None,
-            url = calendar_url,
-            # transparent: bool = None,
-            # alarms: Iterable[BaseAlarm] = None,
-            # attendees: Iterable[Attendee] = None,
-            # categories: Iterable[str] = None,
-            # status: str = None,
-            # organizer: Organizer = None,
-            # geo=None,
-            # classification: str = None,
-        )
-        event.make_all_day()
+        try:
+            event = ics.event.Event(
+                name = eventJSON["articleTitle"],
+                begin = cvtStr2Datetime(eventJSON["etcChar6"]),
+                end = cvtStr2Datetime(eventJSON["etcChar7"]),
+                uid = str(eventJSON["articleNo"]),
+                created = cvtInt2Datetime(eventJSON["createDt"]),
+                last_modified = cvtInt2Datetime(eventJSON["updateDt"]),
+                url = "https://www.smu.ac.kr/ko/life/academicCalendar.do", # smu.ac.kr 학사일정 페이지 URL
+            )
+            event.make_all_day()
+            yield event
+        except:
+            print("failed to add an event")
+    return
+
+def buildCalendarICS() -> ics.icalendar.Calendar:
+    calendar =  ics.icalendar.Calendar(creator=ICS_AUTHOR)
+    for event in parseCalendarJSON(loadCalendarJSON(2023)):
         calendar.events.add(event)
     return calendar
 
 def main():
-    calendar = buildCalendarICS(loadCalendarJSON())
-    with open(calendar_ics_path, 'w') as icsFile:
+    calendar = buildCalendarICS()
+    with open(ICS_FILE_OUTPUT_PATH, 'w') as icsFile:
         icsFile.writelines(calendar.serialize_iter())
 
 main()
